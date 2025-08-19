e8efac6558888229e068e23444ddc6437e3b1156
mipha777 <150020119+mipha777@users.noreply.github.com>
2025-08-19 08:24:04 +0800

    Update run.py

--- HousePrice_momnitoring/run.py
+++ HousePrice_momnitoring/run.py
@@ -3,27 +3,27 @@
 @Author   : shaozi
 @Version  : Python 3.12
 @Date     : 2025/8/16 07:15
-@Desc     : 
+@Desc     :  暂时不用 
 """
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from HousePrice_momnitoring.spiders.anjuke import AnjukeSpider
-from HousePrice_momnitoring.spiders.lianjiaPrice import LianjiapriceSpider
-from HousePrice_momnitoring.spiders.fangtianxia import FangtianxiaSpider
-from HousePrice_momnitoring.push_url_redis import url_to_redis
+# from scrapy.crawler import CrawlerProcess
+# from scrapy.utils.project import get_project_settings
+# from HousePrice_momnitoring.spiders.anjuke import AnjukeSpider
+# from HousePrice_momnitoring.spiders.lianjiaPrice import LianjiapriceSpider
+# from HousePrice_momnitoring.spiders.fangtianxia import FangtianxiaSpider
+# from HousePrice_momnitoring.push_url_redis import url_to_redis
 
 
-# 主运行文件
-def run():
-    # 读取项目配置
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(AnjukeSpider)
-    process.crawl(LianjiapriceSpider)
-    process.crawl(FangtianxiaSpider)
-    # 启动
-    process.start()
+# # 主运行文件
+# def run():
+#     # 读取项目配置
+#     process = CrawlerProcess(get_project_settings())
+#     process.crawl(AnjukeSpider)
+#     process.crawl(LianjiapriceSpider)
+#     process.crawl(FangtianxiaSpider)
+#     # 启动
+#     process.start()
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    run()
+#     run()
